7f62ac52762d29324f7806678e14fe394b42528c
Cris Mendonça <60946367+CrisMendonca@users.noreply.github.com>
2020-05-06 09:36:57 -0300

    Delete install_ansible.sh

--- install_ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Install Ansible"
-sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install ansible -y
-
